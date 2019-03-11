01f3e0f41795969c29094b9beeeb5369ba344004
Vincent Waters <vwaters18@gmail.com>
2019-03-10 21:59:39 -0500

    Test

--- CharacterBuilder/Program.cs
+++ CharacterBuilder/Program.cs
@@ -17,6 +17,7 @@ namespace CharacterBuilder
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainMenu());
+            
         }
     }
 }
